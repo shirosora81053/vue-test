d1b30c8286dd21da5297dc0de78071c9ec892c04
shirosora81053 <shiroganesora@outlook.jp>
2026-03-24 21:22:18 +0900

    不要な証明書の参照を削除する

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next/resolvers'
-import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,12 +19,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('./home-app-local.key'),
-      cert: fs.readFileSync('./home-app-local.crt'),
     },
   },
 })
